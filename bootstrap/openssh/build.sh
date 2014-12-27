1956db049410b3bfaa97ad530d30ee0beb1a85a5
Todd Mortimer <todd@opennet.ca>
2014-12-26 20:45:41 -0500

    openssh

---
+++ bootstrap/openssh/build.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# This code Copyright 2012 Todd Mortimer <todd.mortimer@gmail.com>
+#
+# You may do whatever you like with this code, provided the above
+# copyright notice and this paragraph are preserved.
+
+
+set -e
+source ../../lib.sh
+TASK=fetch
+
+DISTVER="openssh-6.7p1"
+DISTSUFFIX="tar.gz"
+DISTFILES="http://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/$DISTVER.$DISTSUFFIX"
+UNPACKCOMD="tar -xzf"
+package_init "$@"
+CONFIGURE_CMD="./configure 
+                --host=$PBHOSTARCH
+                --build=$PBBUILDARCH 
+                --target=$PBTARGETARCH 
+                --prefix=$PREFIX 
+                --disable-etc-default-login
+                --with-pid-dir=$PREFIX/var/run
+                --with-privsep-path=$PREFIX/var/empty
+                --with-sandbox=no
+                --disable-strip
+                CC=$PBTARGETARCH-gcc 
+                AR=$PBTARGETARCH-ar 
+                CFLAGS="-I$PWD"
+                "
+package_fetch
+package_patch
+package_build
+package_install
+package_bundle
+
+
