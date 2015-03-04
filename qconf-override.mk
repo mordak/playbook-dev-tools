f35940582193202ab2644a55fca975bae32e3731
A. V. Lukyanov <dukzcry@users.noreply.github.com>
2015-03-04 10:09:31 +0300

    pkgsrc packet for which this hack was added got fixed, but we're still preserving it, though setting proper arch now

--- qconf-override.mk
+++ qconf-override.mk
@@ -2,10 +2,10 @@ CP_HOST=/bin/cp -vfpc $(CP_HOST_OPTIONS)
 
 COMPILER_DRIVER= # qcc not yet
 
-CC_nto_x86_gcc=gcc.pkgsrc -c
-AS_nto_x86_gcc=gcc.pkgsrc -c
-AR_nto_x86_gcc=ar
-LR_nto_x86_gcc=gcc.pkgsrc -r
-LD_nto_x86_gcc=gcc.pkgsrc
-UM_nto_x86_gcc=true # usemsg not yet
-OC_nto_x86_gcc=objcopy
+CC_nto_arm_gcc=gcc.pkgsrc -c
+AS_nto_arm_gcc=gcc.pkgsrc -c
+AR_nto_arm_gcc=ar
+LR_nto_arm_gcc=gcc.pkgsrc -r
+LD_nto_arm_gcc=gcc.pkgsrc
+UM_nto_arm_gcc=true # usemsg not yet
+OC_nto_arm_gcc=objcopy
