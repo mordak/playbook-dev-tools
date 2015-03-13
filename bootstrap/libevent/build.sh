d7a6dc3a1486b7dc9854cc24740d9161bb76870e
Todd Mortimer <todd@opennet.ca>
2015-03-12 20:07:22 -0400

    Add libevent and static tmux build

---
+++ bootstrap/libevent/build.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# This code Copyright 2012 Todd Mortimer <todd.mortimer@gmail.com>
+#
+# You may do whatever you like with this code, provided the above
+# copyright notice and this paragraph are preserved.
+
+
+set -e
+source ../../lib.sh
+TASK=fetch
+
+DISTVER="libevent-2.0.22-stable"
+DISTSUFFIX="tar.gz"
+DISTFILES="http://sourceforge.net/projects/levent/files/libevent/libevent-2.0/$DISTVER.$DISTSUFFIX"
+UNPACKCOMD="tar -xzf"
+
+package_init "$@"
+CONFIGURE_CMD="./configure 
+                --host=$PBHOSTARCH
+                --build=$PBBUILDARCH 
+                --target=$PBTARGETARCH 
+                --prefix=$PREFIX 
+                CC=$PBTARGETARCH-gcc 
+                CFLAGS=\"-I$PWD\"
+                "
+
+package_fetch
+package_patch
+package_build
+package_install
+package_bundle
+
---
+++ bootstrap/tmux/build.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# This code Copyright 2012 Todd Mortimer <todd.mortimer@gmail.com>
+#
+# You may do whatever you like with this code, provided the above
+# copyright notice and this paragraph are preserved.
+
+
+set -e
+source ../../lib.sh
+TASK=fetch
+
+DISTVER="tmux-1.9a"
+DISTSUFFIX="tar.gz"
+DISTFILES="http://downloads.sourceforge.net/tmux/$DISTVER.$DISTSUFFIX"
+UNPACKCOMD="tar -xzf"
+LIBEVENT_DIR="libevent-2.0.22-stable"
+
+package_init "$@"
+# we must have build libevent first - see CFLAGS and LDFLAGS below
+CONFIGURE_CMD="./configure 
+                --host=$PBHOSTARCH
+                --build=$PBBUILDARCH 
+                --target=$PBTARGETARCH 
+                --prefix=$PREFIX 
+                --enable-static
+                CC=$PBTARGETARCH-gcc 
+                CFLAGS=\"-I$PWD -I$ARCHIVEDIR/$LIBEVENT_DIR/$PREFIX/include\"
+                LDFLAGS=\"-L$ARCHIVEDIR/$LIBEVENT_DIR/$PREFIX/lib\"
+                PKG_CONFIG_PATH=\"$ARCHIVEDIR/$LIBEVENT_DIR/$PREFIX/lib/pkgconfig/\"
+                LIBEVENT_LIBS=\"-levent -lsocket\"
+                "
+
+package_fetch
+package_patch
+package_build
+package_install
+package_bundle
+
