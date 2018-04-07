422da61188d2cbdea9dda0c24b0f495374d886e8
Olivier Kaloudoff <olivier.kaloudoff@gmail.com>
2018-04-07 13:18:05 +0200

    - added bootstrap/zlib/vars.sh bootstrap/zlib/build.sh - zlib is a dependency for tg (cli telegram client)

---
+++ bootstrap/zlib/build.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# This code Copyright 2012 Todd Mortimer <todd.mortimer@gmail.com>
+#
+# You may do whatever you like with this code, provided the above
+# copyright notice and this paragraph are preserved.
+
+set -e
+source ../../lib.sh
+source ./vars.sh
+TASK=fetch
+
+DISTFILES="https://zlib.net/$DISTVER.$DISTSUFFIX"
+UNPACKCOMD="tar -xzf"
+package_init "$@"
+CONFIGURE_CMD="
+		CC=arm-unknown-nto-qnx8.0.0eabi-gcc
+		AR=arm-unknown-nto-qnx8.0.0eabi-ar
+		LD=arm-unknown-nto-qnx8.0.0eabi-ld
+		./configure 
+                --prefix=$PREFIX 
+                "
+
+#MYMAKEFLAGS='CC=arm-unknown-nto-qnx8.0.0eabi-gcc AR=arm-unknown-nto-qnx8.0.0eabi-ar LD=arm-unknown-nto-qnx8.0.0eabi-ld'
+
+
+package_fetch
+package_patch 1
+package_build
+package_install
+package_bundle
+
---
+++ bootstrap/zlib/vars.sh
@@ -0,0 +1,2 @@
+DISTVER="zlib-1.2.11"
+DISTSUFFIX="tar.gz"
