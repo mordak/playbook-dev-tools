70cd8126982c6096166b24d14866fdf7a732856c
Olivier Kaloudoff <olivier.kaloudoff@gmail.com>
2018-04-13 18:25:57 +0200

    - add xz-5.2.3

---
+++ bootstrap/xz/build.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# This code Copyright 2012 Todd Mortimer <todd.mortimer@gmail.com>
+#
+# You may do whatever you like with this code, provided the above
+# copyright notice and this paragraph are preserved.
+#
+# Olivier Kaloudoff <olivier.kaloudoff@gmail.com>, 2018
+#
+
+
+set -e
+source ../../lib.sh
+source ./vars.sh
+TASK=fetch
+
+
+DISTFILES="https://tukaani.org/xz/$DISTVER.$DISTSUFFIX"
+UNPACKCOMD="tar -xzf"
+TASK=fetch
+package_init "$@"
+CONFIGURE_CMD="./configure 
+                --host=$PBHOSTARCH
+                --build=$PBBUILDARCH 
+                --target=$PBTARGETARCH 
+                --prefix=$PREFIX 
+                --disable-nls 
+                --enable-threads=posix 
+                CC=$PBTARGETARCH-gcc
+                "
+
+package_fetch
+package_patch
+package_build
+package_install
+package_bundle
+
---
+++ bootstrap/xz/vars.sh
@@ -0,0 +1,2 @@
+DISTVER="xz-5.2.3"
+DISTSUFFIX="tar.gz"
