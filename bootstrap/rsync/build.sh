c013da3a4d9a678d8f099b68d2ccb7e16364a3a8
Vasily Mikhaylichenko <vaskas@lxmx.com.au>
2019-12-17 02:44:26 +1100

    Fix the broken rsync build

--- bootstrap/rsync/build.sh
+++ bootstrap/rsync/build.sh
@@ -17,40 +17,18 @@ DISTFILES="https://download.samba.org/pub/rsync/src/$DISTVER.$DISTSUFFIX"
 UNPACKCOMD="tar -xzf"
 TASK=fetch
 package_init "$@"
-CONFIGURE_CMD="./configure 
+CONFIGURE_CMD="./configure
                 --host=$PBHOSTARCH
-                --build=$PBBUILDARCH 
-                --target=$PBTARGETARCH 
-                --prefix=$PREFIX 
+                --build=$PBBUILDARCH
+                --target=$PBTARGETARCH
+                --prefix=$PREFIX
                 CC=$PBTARGETARCH-gcc
 		CFLAGS=-O3
                 "
 
 package_fetch
 package_patch
-
-if [ "$TASK" == "build" ]
-then
-  echo "Building"
-  cd "$WORKDIR"
-  # clean up if we have a previous build
-  #if [ -e "Makefile" ]; then
-  #  make clean || true
-  #  make distclean || true
-  #fi
-  # configure
-  eval $CONFIGURE_CMD
-
-  for apatch in $EXECDIR/post-conf-patches/*
-   do
-	patch -p0 < $apatch
-   done
-
-  eval $MAKE_PREFIX make $MYMAKEFLAGS || \
-        eval $MAKE_PREFIX make
-  TASK=install
-fi
-
+package_build
 package_install
 package_bundle
 
