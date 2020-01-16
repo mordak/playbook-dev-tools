b0f3004cc3ed131261e62aac8541581c41b2ba36
Olivier Kaloudoff <kaloudov@yandex.com>
2020-01-17 00:59:35 +0100

    ports-wip/autoconf/build.sh: builds fine

--- ports-wip/autoconf/build.sh
+++ ports-wip/autoconf/build.sh
@@ -31,30 +31,8 @@ CONFIGURE_CMD="./configure
                 "
 
 package_fetch
-#package_patch
-
-if [ "$TASK" == "build" ]
-then
-  echo "Building"
-  cd "$WORKDIR"
-  # clean up if we have a previous build
-  #if [ -e "Makefile" ]; then
-  #  make clean || true
-  #  make distclean || true
-  #fi
-  # configure
-  eval $CONFIGURE_CMD
-
-  for apatch in $EXECDIR/post-conf-patches/*
-   do
-	patch -p0 < $apatch
-   done
-
-  eval $MAKE_PREFIX make $MYMAKEFLAGS || \
-        eval $MAKE_PREFIX make
-  TASK=install
-fi
-
+package_patch
+package_build
 package_install
 package_bundle
 
