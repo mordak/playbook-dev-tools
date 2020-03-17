9f40b7fe4d3678bc89d3bb6a15f3c8e5f348b2a9
Olivier Kaloudoff <kaloudov@yandex.com>
2020-03-17 23:53:57 +0100

    ports-wip/autossh: fixes compilation issue (execution fails silently)

--- ports-wip/autossh/build.sh
+++ ports-wip/autossh/build.sh
@@ -24,7 +24,9 @@ CONFIGURE_CMD="
                 CC=$PBTARGETARCH-gcc 
 		AR=arm-unknown-nto-qnx8.0.0eabi-ar
 		LD=arm-unknown-nto-qnx8.0.0eabi-ld
-		./configure 
+		 ac_cv_func_malloc_0_nonnull=yes
+		 ac_cv_func_realloc_0_nonnull=yes ./configure 
+		--with-gnu-ld
                 --prefix=$PREFIX 
                 --host=$PBHOSTARCH
                 --build=$PBBUILDARCH 
@@ -33,6 +35,7 @@ CONFIGURE_CMD="
 package_fetch
 package_patch 1
 cp $EXECDIR/syslog.h $WORKDIR/
+#cp $EXECDIR/rpl_malloc.c $WORKDIR/
 package_build
 package_install
 package_bundle
