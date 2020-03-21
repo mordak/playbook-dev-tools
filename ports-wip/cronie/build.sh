e0acea87a3a7473f7d502a1cad431f0380b98dbe
Olivier Kaloudoff <kaloudov@yandex.com>
2020-03-21 01:10:53 +0100

    ports-wip/cronie: debug build

--- ports-wip/cronie/build.sh
+++ ports-wip/cronie/build.sh
@@ -10,18 +10,19 @@ set -e
 source ../../lib.sh
 TASK=fetch
 
-DISTVER="cronie-1.5.2"
+DISTVER="cronie-1.5.5"
 DISTSUFFIX="tar.gz"
-DISTFILES="https://github.com/cronie-crond/cronie/releases/download/cronie-1.5.2/$DISTVER.$DISTSUFFIX"
+DISTFILES="https://github.com/cronie-crond/cronie/releases/download/cronie-1.5.5/$DISTVER.$DISTSUFFIX"
 UNPACKCOMD="tar -xzf"
 package_init "$@"
 CONFIGURE_CMD="./configure
-		CFLAGS=\"-fPIC\"
+		CFLAGS=\"-fPIC -g -ggdb -fvar-tracking\"
                 --host=$PBHOSTARCH
                 --build=$PBBUILDARCH
                 --target=$PBTARGETARCH
                 --prefix=$PREFIX
 		--localstatedir=/var/tmp
+		--disable-anacron
                 CC=$PBTARGETARCH-gcc
                 "
 
