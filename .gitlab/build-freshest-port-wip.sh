c242c47c89cf75dac2045dbdb8755de2de558a90
Olivier Kaloudoff <kaloudov@yandex.com>
2020-03-21 10:15:36 +0100

    .gitlab/build-freshest-port-wip.sh

--- .gitlab/build-freshest-port-wip.sh
+++ .gitlab/build-freshest-port-wip.sh
@@ -5,6 +5,5 @@ PFX=/berrymuch
 # find last updated WIP port
 PORT_NAME=$(ls -ltr $PFX/ports-wip/ | awk 'END { split($0, a, " "); print a[NF] }')
 
-
 pushd $PFX/ports-wip/$PORT_NAME
 bash -x ./build.sh
