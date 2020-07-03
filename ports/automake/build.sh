6d934110b7f5e91af5b3ea7397e6f5177cbdd88e
Efek <efek0349@protonmail.com>
2020-07-03 08:31:57 +0200

    add automake

---
+++ ports/automake/build.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# This code Copyright 2012 Todd Mortimer <todd.mortimer@gmail.com>
+#
+# You may do whatever you like with this code, provided the above
+# copyright notice and this paragraph are preserved.
+#
+
+
+
+set -e
+source ../../lib.sh
+TASK=fetch
+
+DISTVER="automake-1.16.2"
+DISTSUFFIX="tar.gz"
+
+
+DISTFILES="https://ftp.gnu.org/gnu/automake/$DISTVER.$DISTSUFFIX"
+UNPACKCOMD="tar -xzf"
+TASK=fetch
+package_init "$@"
+CONFIGURE_CMD="./configure 
+                --host=$PBHOSTARCH
+                --build=$PBBUILDARCH 
+                --target=$PBTARGETARCH 
+                --prefix=$PREFIX 
+                CC=$PBTARGETARCH-gcc
+		CFLAGS=-O3
+                "
+
+package_fetch
+package_patch
+package_build
+package_install
+package_bundle
+
