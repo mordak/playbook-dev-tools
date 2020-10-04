0f0857c259e88d196e496c7d6ef13291195efd8c
Olivier Kaloudoff <kaloudov@yandex.com>
2020-10-04 21:14:09 +0200

    ports-wip/yank, v1.2.0

---
+++ ports-wip/yank/build.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# This code Copyright 2012 Todd Mortimer <todd.mortimer@gmail.com>
+#
+# You may do whatever you like with this code, provided the above
+# copyright notice and this paragraph are preserved.
+#
+
+
+set -e
+source ../../lib.sh
+DISTVER="yank"
+DISTSUFFIX="git"
+TASK=fetch
+
+DISTFILES="https://github.com/mptre/$DISTVER.$DISTSUFFIX"
+GITTAG=v1.2.0
+
+#CONFIGURE_CMD=""
+MYMAKEFLAGS="   CC=$PBTARGETARCH-gcc 
+		CPPFLAGS=\"-I. -Iinclude\"
+		"
+
+package_init "$@"
+# No configure, just make
+
+if [ "$TASK" == "fetch" ]
+then
+  cd "$WORKROOT"
+  rm -rf fakeroot
+  git clone $DISTFILES --depth 1
+  cd $DISTVER
+  git fetch --tags
+  git checkout $GITTAG
+
+  TASK=build
+fi
+
+#package_patch
+package_build
+package_install
+package_bundle
+
